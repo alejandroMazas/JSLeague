b32a668e6cb4c978c2f964b9b245b18a83856ea7
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2023-02-23 13:25:09 +0100

    start classes

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const user = "alejandro"
-
-const hello = (name) => {
-    console.log(`hola ${name}`)
+class League {
+    constructor(name, teams,) {
+        this.name = name
+        this.teams = teams
+        this.rounds = rounds
+        this.matches = []
+        this.matchSchedule = []
+        this.scores = []
+    }
 }
 
-hello(user)
+class FootbalLeague extends League {
+    constructor(name, teams, rounds = 1, pointsPerWin = 3, pointsPerLose = 0, pointsPerDraw = 1) {
+        super(name, teams, rounds)
+        this.pointsPerWin = pointsPerWin
+        this.pointsPerDraw = pointsPerDraw
+        this.pointsPerLose = pointsPerLose
+
+    }
+}
+
+
+class Team {
+    constructor(teamName, wins, losses) {
+        this.teamName = teamName
+        this.wins = wins
+        this.losses = losses
+    }
+
+}
