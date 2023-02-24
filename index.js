e3048f81addd2589187ed4d998d75dfb35e7c897
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2023-02-24 07:06:32 +0100

    teams config

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const teams = [
+    "Duke",
+    "Shelly",
+    "Caleb",
+    "Lo Wang"
+]
+
 class League {
     constructor(name, teams, config = {}) {
         this.name = name
-        this.teams = teams
-
         this.setup(config)
+        this.setupTeams(teams)
         this.matches = []
         this.matchSchedule = []
         this.scores = []
@@ -13,11 +19,35 @@ class League {
         const defaultConfig = { rounds: 1 }
         this.config = Object.assign(defaultConfig, config)
     }
+
+    setupTeams(teams) {
+        this.teams = []
+        for (let team of teams) {
+            let teamObj = this.customizeTeam(teams)
+            let teamObj = {
+                name: team,
+                matchesWon: 0,
+                matchesDraw: 0,
+                matchesLost: 0
+            }
+            this.teams.push(teamObj)
+        }
+    }
+
+    customizeTeam(teams) {
+        return {
+            name: team,
+            matchesWon: 0,
+            matchesDraw: 0,
+            matchesLost: 0
+        }
+    }
+
 }
 
 class FootbalLeague extends League {
     constructor(name, teams, config) {
-        super(name, teams, rounds)
+        super(name, teams,)
     }
 
     setup(config = {}) {
@@ -30,15 +60,19 @@ class FootbalLeague extends League {
 
         this.config = Object.assign(defaultConfig, config)
     }
-}
 
+    customizeTeam(teamName) {
+        const teamObj = super.customizeTeam(teamName)
 
-class Team {
-    constructor(teamName, wins, losses) {
-        this.teamName = teamName
-        this.wins = wins
-        this.losses = losses
+        return {
+            ...teamObj,
+            points: 0,
+            goalsFor: 0,
+            goalsAgainst: 0
+        }
     }
-
 }
+
+const miLiga = new FootbalLeague("ProfessionalLeague", teams)
+console.log(miLiga.teams)
 
